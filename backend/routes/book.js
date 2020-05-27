f323a91259fe40c3209940e9294c36436aefa328
Valentin Vivaldi <valenvivaldi@gmail.com>
2020-05-27 10:19:21 -0300

    pagination on server

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,20 +1,20 @@
-'use strict'
-var express = require('express');
-var BookController = require('../controllers/book');
+"use strict";
+var express = require("express");
+var BookController = require("../controllers/book");
 
 //importamos libreria de connect-multiparty
-var multipart = require('connect-multiparty');
-var md_upload = multipart({uploadDir: './files'});
-
+var multipart = require("connect-multiparty");
+var md_upload = multipart({ uploadDir: "./files" });
 
 //creamos un router
 var router = express.Router();
-router.get('/test',BookController.test);
+router.get("/test", BookController.test);
 
-router.get('/',BookController.all);
+router.get("/", BookController.all);
 router.get("/search/:searchTerm", BookController.search);
 
+router.get("/quantity", BookController.quantity);
 
-router.post('/importFromExcel',md_upload,BookController.importFromExcel); 
+router.post("/importFromExcel", md_upload, BookController.importFromExcel);
 
-module.exports=router;
+module.exports = router;
