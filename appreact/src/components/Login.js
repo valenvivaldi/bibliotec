92927e491993906f6bb0cd56f2a220ec9768901b
Valentin Vivaldi <valenvivaldi@gmail.com>
2020-06-08 20:54:40 -0300

    progress

--- appreact/src/components/Login.js
+++ appreact/src/components/Login.js
@@ -1,27 +1,78 @@
 import React, { Component } from "react";
-import axios from "axios";
-import Global from "../Global";
-class Login extends Component {
-  state = {};
 
+import Global from "../Global";
+
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../actions/authActions";
+
+class Login extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/"); // push user to dashboard when they login
+    }
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors,
+      });
+    }
+  }
+  state = {};
+  onChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+  };
+
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/");
+    }
+  }
   render() {
     return (
       <React.Fragment>
-        <form className="formLogin">
+        <form className="formLogin" onSubmit={this.onSubmit}>
           <form-group></form-group>
           <label htmlFor="dni" className="labelLogin">
             DNI
           </label>
-          <input type="text" name="dni" className="inputLogin"></input>
+          <input
+            type="text"
+            name="dni"
+            className="inputLogin"
+            onChange={this.onChange}
+          ></input>
           <label htmlFor="password" className="labelLogin">
             Contraseña
           </label>
-          <input type="password" name="password" className="inputLogin"></input>
-          <input type="submit" className='submitLogin'></input>
+          <input
+            type="password"
+            name="password"
+            className="inputLogin"
+            onChange={this.onChange}
+          ></input>
+          <input type="submit" className="submitLogin"></input>
         </form>
         {this.state.message && <p className="pmessage">{this.state.message}</p>}
       </React.Fragment>
     );
   }
 }
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors,
+});
+export default connect(mapStateToProps, { loginUser })(Login);
--- appreact/src/components/Router.js
+++ appreact/src/components/Router.js
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import BookDisplay from "./BooksDisplay";
-import UploadExcel from './UploadExcel';
+import UploadExcel from "./UploadExcel";
 import Login from "./Login";
 import Register from "./Register";
+import Header from "./Header";
 
 class Router extends Component {
   render() {
     return (
       //rutas y paginas
-      <div className="center">
-        <div className="content">
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={BookDisplay} />
-              <Route path="/uploadExcel" component={UploadExcel} />
-              <Route path="/login" component={Login} />
-              <Route path="/register" component={Register} />
-            </Switch>
-          </BrowserRouter>
-        </div>
-      </div>
+      
+        <BrowserRouter>
+        <Header />
+          <Switch>
+            <div className="center">
+              <div className="content">
+                <Route exact path="/" component={BookDisplay} />
+                <Route path="/uploadExcel" component={UploadExcel} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+              </div>
+            </div>
+          </Switch>
+        </BrowserRouter>
+      
     );
   }
 }
